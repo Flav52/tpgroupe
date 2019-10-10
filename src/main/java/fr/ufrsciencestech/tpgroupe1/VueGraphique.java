8cc9f877f333146f4d3e3363528064258f7597ed
Alexandre SCHIRMANN <as548061@MI105-15.iem>
2019-10-10 10:56:51 +0200

    debut IHM

--- src/main/java/fr/ufrsciencestech/tpgroupe1/VueGraphique.java
+++ src/main/java/fr/ufrsciencestech/tpgroupe1/VueGraphique.java
@@ -5,6 +5,7 @@ import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 
 public class VueGraphique extends javax.swing.JFrame implements Observer {
 
@@ -25,10 +26,11 @@ public class VueGraphique extends javax.swing.JFrame implements Observer {
     
     @Override
     public void update(Observable o, Object arg) {
+        
         affiche.setText(Integer.toString(((Panier) o).getNbElements()));
     }
 
-    public JLabel getAffiche() {
+    public JTextArea getAffiche() {
         return affiche;
     }
 
@@ -40,7 +42,7 @@ public class VueGraphique extends javax.swing.JFrame implements Observer {
         return inc;
     }  
     
-    public void setAffiche(JLabel affiche) {
+    public void setAffiche(JTextArea affiche) {
         this.affiche = affiche;
     }
     
@@ -49,31 +51,45 @@ public class VueGraphique extends javax.swing.JFrame implements Observer {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
         inc = new javax.swing.JButton();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jPanel4 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        affiche = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        affiche = new javax.swing.JTextArea();
         jPanel3 = new javax.swing.JPanel();
         dec = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
+        getContentPane().setLayout(new java.awt.BorderLayout());
 
-        jPanel1.setPreferredSize(new java.awt.Dimension(400, 100));
-        jPanel1.setLayout(new java.awt.GridLayout(1, 1));
+        jPanel1.setPreferredSize(new java.awt.Dimension(400, 40));
+        jPanel1.setLayout(new java.awt.GridLayout(1, 4));
+        jPanel1.add(jPanel5);
 
         inc.setText("+");
-        inc.setMaximumSize(new java.awt.Dimension(39, 25));
-        inc.setMinimumSize(new java.awt.Dimension(39, 25));
-        inc.setPreferredSize(new java.awt.Dimension(39, 25));
+        inc.setMaximumSize(new java.awt.Dimension(100, 100));
+        inc.setMinimumSize(new java.awt.Dimension(0, 0));
+        inc.setPreferredSize(new java.awt.Dimension(30, 25));
         jPanel1.add(inc);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jPanel1.add(jComboBox1);
+        jPanel1.add(jPanel4);
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.NORTH);
 
         jPanel2.setPreferredSize(new java.awt.Dimension(400, 200));
         jPanel2.setLayout(new java.awt.GridLayout(1, 1));
 
-        affiche.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jPanel2.add(affiche);
+        affiche.setEditable(false);
+        affiche.setColumns(20);
+        affiche.setRows(5);
+        jScrollPane1.setViewportView(affiche);
+
+        jPanel2.add(jScrollPane1);
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.CENTER);
 
@@ -120,12 +136,16 @@ public class VueGraphique extends javax.swing.JFrame implements Observer {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel affiche;
+    private javax.swing.JTextArea affiche;
     private javax.swing.JButton dec;
     private javax.swing.JButton inc;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
 
